6a88ead1aaf95b2b6600457b229bc8b364e1de7b
NOCTEM <121877002+ktplick@users.noreply.github.com>
2025-04-04 21:59:07 +0200

    Update numberAnimate.js

--- numberAnimate.js
+++ numberAnimate.js
@@ -1,10 +1,3 @@
-/*global define:false, WebKitCSSMatrix:false */
-/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, evil:true, 
-    laxbreak:true, bitwise:true, strict:true, undef:true, unused:true, browser:true,
-    jquery:true, indent:4, curly:false, maxerr:50 */
-
-//Set the plugin up so that it'll work as a AMD module or regular import
-//See: https://github.com/umdjs/umd/blob/master/jqueryPlugin.js..
 (function (factory) {
     "use strict";
     if (typeof define === 'function' && define.amd) {
